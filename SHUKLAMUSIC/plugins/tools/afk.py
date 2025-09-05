ed7c2a824fc532f02f872cb6d74fdac6d30e6f23
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-09-05 09:25:22 +0530

    Update afk.py

--- SHUKLAMUSIC/plugins/tools/afk.py
+++ SHUKLAMUSIC/plugins/tools/afk.py
@@ -1,12 +1,12 @@
 import time, re
 from config import BOT_USERNAME
-from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
 from SHUKLAMUSIC import app
 from SHUKLAMUSIC.mongo.readable_time import get_readable_time
 from SHUKLAMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
 
+# --- AFK SET COMMANDS ---
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
 async def active_afk(_, message: Message):
     if message.sender_chat:
@@ -39,22 +39,25 @@ async def active_afk(_, message: Message):
     await add_afk(user_id, details)
     await message.reply_text(f"{message.from_user.first_name} ɪs ɴᴏᴡ ᴀғᴋ!")
 
+# --- CHAT WATCHER ---
 chat_watcher_group = 1
 
 @app.on_message(
     ~filters.me & ~filters.bot & ~filters.via_bot,
     group=chat_watcher_group,
 )
-async def chat_watcher_func(_, message):
-    if message.sender_chat:
+async def chat_watcher_func(_, message: Message):
+    if message.sender_chat or not message.from_user:
         return
+
     userid = message.from_user.id
     user_name = message.from_user.first_name
     msg = ""
-    replied_user_id = 0
 
-    # Ignore AFK check if command is /afk or /brb
-    if message.text and message.text.split()[0].lower() not in ["/afk", f"/afk@{BOT_USERNAME}", "/brb", f"/brb@{BOT_USERNAME}"]:
+    # --- Remove AFK on ANY message (text, media, sticker, etc.) ---
+    if not message.text or message.text.split()[0].lower() not in [
+        "/afk", f"/afk@{BOT_USERNAME}", "/brb", f"/brb@{BOT_USERNAME}"
+    ]:
         verifier, reasondb = await is_afk(userid)
         if verifier:
             await remove_afk(userid)
@@ -62,7 +65,7 @@ async def chat_watcher_func(_, message):
                 afktype = reasondb["type"]
                 timeafk = reasondb["time"]
                 reasonafk = reasondb["reason"]
-                seenago = get_readable_time((int(time.time() - timeafk)))
+                seenago = get_readable_time(int(time.time() - timeafk))
                 if afktype == "text":
                     msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ ᴀɴᴅ ᴡᴀs ᴀᴡᴀʏ ғᴏʀ {seenago}\n\n"
                 if afktype == "text_reason":
@@ -70,8 +73,8 @@ async def chat_watcher_func(_, message):
             except:
                 msg += f"**{user_name[:25]}** ɪs ʙᴀᴄᴋ ᴏɴʟɪɴᴇ\n\n"
 
-    # Handle reply AFK detection
-    if message.reply_to_message:
+    # --- Check if replied user is AFK ---
+    if message.reply_to_message and message.reply_to_message.from_user:
         try:
             replied_first_name = message.reply_to_message.from_user.first_name
             replied_user_id = message.reply_to_message.from_user.id
@@ -80,7 +83,7 @@ async def chat_watcher_func(_, message):
                 afktype = reasondb["type"]
                 timeafk = reasondb["time"]
                 reasonafk = reasondb["reason"]
-                seenago = get_readable_time((int(time.time() - timeafk)))
+                seenago = get_readable_time(int(time.time() - timeafk))
                 if afktype == "text":
                     msg += f"**{replied_first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
                 if afktype == "text_reason":
@@ -88,7 +91,7 @@ async def chat_watcher_func(_, message):
         except:
             pass
 
-    # Detect plain @username mentions in text
+    # --- Check mentions ---
     if message.entities:
         text = message.text or message.caption or ""
         mentions = re.findall(r"@([_0-9a-zA-Z]+)", text)
@@ -102,7 +105,7 @@ async def chat_watcher_func(_, message):
                     afktype = reasondb["type"]
                     timeafk = reasondb["time"]
                     reasonafk = reasondb["reason"]
-                    seenago = get_readable_time((int(time.time() - timeafk)))
+                    seenago = get_readable_time(int(time.time() - timeafk))
                     if afktype == "text":
                         msg += f"**{user.first_name[:25]}** ɪs ᴀғᴋ sɪɴᴄᴇ {seenago}\n\n"
                     if afktype == "text_reason":
@@ -110,7 +113,7 @@ async def chat_watcher_func(_, message):
             except:
                 continue
 
-    if msg != "":
+    if msg:
         try:
             await message.reply_text(msg, disable_web_page_preview=True)
         except:
