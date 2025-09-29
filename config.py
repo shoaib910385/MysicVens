fd89853674bdbf62019cc99a8d92cf5e230b1e20
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-09-29 12:24:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # Git
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/shoaib910385/MysicVen")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/shoaib910385/MysicVens")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
