88d25e9b2d4cdc7ad01c2849752780ce6d854327
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-09-29 12:01:01 +0530

    Update Gpt.py

--- SHUKLAMUSIC/plugins/tools/Gpt.py
+++ SHUKLAMUSIC/plugins/tools/Gpt.py
@@ -68,7 +68,7 @@ async def chat_gpt(bot, message):
                     if "choices" in response_data and len(response_data["choices"]) > 0:
                         result = response_data["choices"][0]["message"]["content"]
                         await message.reply_text(
-                            f"{result} \n\nＡɴsᴡᴇʀᴇᴅ ʙʏ➛[愛|| ❰𝗗𝗥𝗫❱™ ɴᴇᴛᴡᴏʀᴋ ||](https://t.me/thedrxnet)",
+                            f"{result} \n\nＡɴsᴡᴇʀᴇᴅ ʙʏ➛[❰𝕍𝕋ℍ❱™ 𝐍𝐄𝐓𝐖𝐎𝐑𝐊 ||](https://t.me/Imvalrik)",
                             parse_mode=ParseMode.MARKDOWN
                         )
                     else:
